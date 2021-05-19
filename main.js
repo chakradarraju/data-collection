20af23b5ed8067a4535d36d81ac38fa345e8b361
Chakradar Raju <chakradarraju@gmail.com>
2021-05-19 23:48:03 +0530

    Human readable time format

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const date = document.getElementById("date");
 
-date.value = new Date().toISOString();
+const dt = new Date();
+const hrs = 1 + ((dt.getHours() + 11) % 12);
+const hours = (hrs < 10 ? '0' : '') + hrs;
+const minutes = (dt.getMinutes() < 10 ? '0' : '') + dt.getMinutes();
+date.value = dt.toISOString().substr(0, 10) + " " + hours + ":" + minutes + " " + (dt.getHours() < 12 ? "AM" : "PM");
+(() => {
+  window.onload = () => {
+    const eles = Array.from(form.elements);
+    console.log('l', eles.length);
+  }
+})();
 
 const form = document.forms.patient_form;
 
@@ -16,7 +26,7 @@ form.onsubmit = (ev) => {
 
   patient_data.push("\n");
 
-  formElements.slice(1).forEach((e, i) => {
+  formElements.slice(1, -1).forEach((e, i) => {
     patient_data.push(`${i + 1}) ${e.name}: ${e.value}`);
   });
 
