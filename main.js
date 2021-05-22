75210212c0103106fabd60cb4c09ad8203560460
Chakradar Raju <chakradarraju@gmail.com>
2021-05-22 16:17:56 +0530

    Add whatsapp link to phone number fields

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-var date, regNameEle;
+var date, regNameEle, formTitle = null;
 
 (() => {
   window.onload = () => {
     date = document.getElementById("date");
     regNameEle = document.getElementById('registered-by');
+    titleEle = document.getElementById('form-title');
+
+    if (titleEle) formTitle = titleEle.innerText;
 
     const regName = localStorage.getItem('register-name');
     if (regName)
@@ -26,18 +29,25 @@ form.onsubmit = (ev) => {
 
   const formElements = Array.from(form.elements);
 
-  const patient_data = [];
+  const patientData = [];
+  if (formTitle !== null) {
+    patientData.push("*" + formTitle + "*");
+    patientData.push("");
+  }
+
   formElements.slice(0, -3).forEach((e, i) => {
-    patient_data.push(`${i + 1}) ${e.name}: ${e.value}`);
+    var line = `${i + 1}) ${e.name}: ${e.value}`;
+    if (e.dataset.type === "phone" && e.value && e.value.length > 0) line += ` https://wa.me/${e.value}`;
+    patientData.push(line);
   });
 
-  patient_data.push('\n');
+  patientData.push('\n');
 
   formElements.slice(-3, -1).forEach((e, i) => {
-    patient_data.push(`${e.name}: ${e.value}`);
+    patientData.push(`${e.name}: ${e.value}`);
   });
 
   window.location.href = `https://api.whatsapp.com/send?text=${encodeURIComponent(
-    patient_data.join("\n")
+    patientData.join("\n")
   )}`;
 };
